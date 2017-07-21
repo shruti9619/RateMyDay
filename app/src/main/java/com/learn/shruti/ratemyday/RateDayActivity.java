afe6f2b8be47b4f836cfbc382ef1acebc91711bb
Shruti Iyyer <shruti.personified@gmail.com>
2017-07-21 22:55:57 +0530

    Business logic handling in add rating activity

--- app/src/main/java/com/learn/shruti/ratemyday/RateDayActivity.java
+++ app/src/main/java/com/learn/shruti/ratemyday/RateDayActivity.java
@@ -10,6 +10,8 @@ import com.google.firebase.database.*;
 import com.learn.shruti.ratemyday.Model.Employee;
 import com.learn.shruti.ratemyday.Model.Review;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -60,40 +62,55 @@ public class RateDayActivity extends AppCompatActivity {
     private void writeRatingsToFirebase()
     {
         user = auth.getCurrentUser();
-        final String userEmail = user.getEmail();
+        try {
 
-        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
-        Query usernamequery = mDatabase.child("users").orderByChild("empEmail").equalTo(userEmail);
-        usernamequery.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-                    //Getting the data from snapshot
-                    Employee r = postSnapshot.getValue(Employee.class);
 
-                    //review is only added once user with auth email is found
-                    if(r.empEmail == userEmail)
-                    {
+            final String userEmail = user.getEmail();
 
-                        DatabaseReference uDatabase = FirebaseDatabase.getInstance().getReference("reviews");
-                        Review newrev = new Review(dayratingbar.getRating(),new Date().toString(),
-                                feedbacktext.getText().toString(),r.empName,userEmail);
+            DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+            Query usernamequery = mDatabase.child("users").orderByChild("empEmail").equalTo(userEmail);
+            usernamequery.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                        //Getting the data from snapshot
+                        Employee r = postSnapshot.getValue(Employee.class);
 
-                        // pushing review to 'reviews' node using the uniqueID
-                        uDatabase.child(uDatabase.push().getKey()).setValue(newrev);
+                        //review is only added once user with auth email is found
+                        if (r.empEmail.equalsIgnoreCase(userEmail)) {
+                            String com = "";
+                            DatabaseReference uDatabase = FirebaseDatabase.getInstance().getReference("reviews");
 
-                        Toast.makeText(RateDayActivity.this,"Saved",Toast.LENGTH_SHORT).show();
+                            //to check whether the user has entered any comments or not and replace empty with NA
+                            if(feedbacktext.getText()!=null)
+                            if (feedbacktext.getText().toString().length()<=0)
+                                com = "NA";
+
+                            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                            Review newrev = new Review(dayratingbar.getRating(), dateFormat.format(new Date()),
+                                    com, r.empName, userEmail);
+
+                            // pushing review to 'reviews' node using the uniqueID
+                            uDatabase.child(uDatabase.push().getKey()).setValue(newrev);
+
+                            Toast.makeText(RateDayActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+
+                        }
 
                     }
+                }
+
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
 
                 }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+            });
+        }
+        catch (Exception e)
+        {
+            //exception caught . no action required
+        }
 
 
     }
