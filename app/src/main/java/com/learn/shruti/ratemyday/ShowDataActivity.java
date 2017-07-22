50f494cb7dc5a629ff751a2b5cc7da0a34115d91
Shruti Iyyer <shruti.personified@gmail.com>
2017-07-22 22:45:10 +0530

    searchview adapter bug fixed

--- app/src/main/java/com/learn/shruti/ratemyday/ShowDataActivity.java
+++ app/src/main/java/com/learn/shruti/ratemyday/ShowDataActivity.java
@@ -115,6 +115,7 @@ public class ShowDataActivity extends AppCompatActivity {
                     else
                     {
                         rvadapter = new ReviewAdapter(reviewsearchList);
+                        mRecyclerView.setAdapter(rvadapter);
                     }
                 }
 
